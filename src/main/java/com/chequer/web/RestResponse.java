30f06ab8a3de0e547a6261ed30e1bf8ea2cda54e
choiyuri <choiyuri02@naver.com>
2021-12-10 13:51:55 +0900

    comment

--- src/main/java/com/chequer/web/RestResponse.java
+++ src/main/java/com/chequer/web/RestResponse.java
@@ -4,6 +4,9 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Builder;
 import lombok.Getter;
 
+/**
+ * 모든 요청에 공통으로 사용되는 Response 객체
+ */
 @JsonInclude(JsonInclude.Include.NON_NULL)
 @Getter
 public class RestResponse {
