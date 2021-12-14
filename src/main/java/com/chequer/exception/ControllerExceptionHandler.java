15f51f1e06a8ab6bc0609f438469ba42cf26bd1f
choiyuri <choiyuri02@naver.com>
2021-12-14 17:34:34 +0900

    에러 HttpStatus 추가

--- src/main/java/com/chequer/exception/ControllerExceptionHandler.java
+++ src/main/java/com/chequer/exception/ControllerExceptionHandler.java
@@ -4,38 +4,43 @@ import com.chequer.web.common.RestResponse;
 import com.chequer.web.common.ResultType;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.lang.Nullable;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import javax.validation.constraints.Null;
 
 @Slf4j
 @RestControllerAdvice
 public class ControllerExceptionHandler {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(BaseException.class)
-    protected RestResponse baseExceptionHandle(BaseException e) {
+    protected ResponseEntity<RestResponse<Nullable>> baseExceptionHandle(BaseException e) {
 
-        log.error("Base Exception : {}", e.getErrorCode().getDetail());
+        log.error("Base Exception : {}", (Object[]) e.getStackTrace());
 
-        return RestResponse.builder()
-                .result(ResultType.FAIL)
-                .error(e.getErrorCode())
-                .build();
+        return ResponseEntity
+                .status(e.getErrorCode().getStatus())
+                .body(RestResponse.<Nullable>builder()
+                        .result(ResultType.FAIL)
+                        .error(e.getErrorCode())
+                        .build());
 
     }
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected RestResponse notValidExceptionHandle(MethodArgumentNotValidException e) {
+    protected ResponseEntity<RestResponse<Nullable>> notValidExceptionHandle(MethodArgumentNotValidException e) {
 
         log.error("Not Valid Exception : {}", e.getBindingResult());
 
-        return RestResponse.builder()
-                .result(ResultType.FAIL)
-                .error(ErrorCode.E0001)
-                .build();
+        return ResponseEntity
+                .status(HttpStatus.BAD_REQUEST)
+                .body(RestResponse.<Nullable>builder()
+                        .result(ResultType.FAIL)
+                        .error(ErrorCode.E0001)
+                        .build());
 
     }
 }
--- src/main/java/com/chequer/exception/ErrorCode.java
+++ src/main/java/com/chequer/exception/ErrorCode.java
@@ -2,31 +2,33 @@ package com.chequer.exception;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import org.springframework.http.HttpStatus;
 
 @Getter
 @AllArgsConstructor
 public enum ErrorCode {
 
     // Valid
-    E0001("입력 값이 유효하지 않습니다."),
+    E0001(HttpStatus.BAD_REQUEST, "입력 값이 유효하지 않습니다."),
 
     // 사용자
-    E1001("이미 등록되어 있는 이메일입니다."),
-    E1002("해당 사용자가 존재하지 않습니다."),
-    E1003("해당 사용자는 권한이 없습니다."),
-    E1004("계정이 활성화 되어 있지 않습니다."),
+    E1001(HttpStatus.BAD_REQUEST,"이미 등록되어 있는 이메일입니다."),
+    E1002(HttpStatus.BAD_REQUEST,"해당 사용자가 존재하지 않습니다."),
+    E1003(HttpStatus.FORBIDDEN,"해당 사용자는 권한이 없습니다."),
+    E1004(HttpStatus.BAD_REQUEST,"계정이 활성화 되어 있지 않습니다."),
 
     // 게시글
-    E2001("해당 게시글이 존재하지 않습니다."),
+    E2001(HttpStatus.OK,"해당 게시글이 존재하지 않습니다."),
 
     // JWT
-    E3001("잘못된 JWT 서명입니다."),
-    E3002("만료된 JWT 토큰입니다."),
-    E3003("지원되지 않는 JWT 토큰입니다."),
-    E3004("JWT 토큰이 잘못되었습니다."),
-    E3005("인증에 실패하였습니다."),
+    E3001(HttpStatus.UNAUTHORIZED,"잘못된 JWT 서명입니다."),
+    E3002(HttpStatus.UNAUTHORIZED,"만료된 JWT 토큰입니다."),
+    E3003(HttpStatus.UNAUTHORIZED,"지원되지 않는 JWT 토큰입니다."),
+    E3004(HttpStatus.UNAUTHORIZED,"JWT 토큰이 잘못되었습니다."),
+    E3005(HttpStatus.UNAUTHORIZED,"인증에 실패하였습니다."),
 
     ;
 
+    private final HttpStatus status;
     private final String detail;
 }
