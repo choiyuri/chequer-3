802a3498b614f17944d7ec6b0b5c8158f3e371df
choiyuri <choiyuri02@naver.com>
2021-12-15 23:24:59 +0900

    fix

--- src/main/java/com/chequer/config/AuditorAwareConfig.java
+++ src/main/java/com/chequer/config/AuditorAwareConfig.java
@@ -1,16 +1,12 @@
 package com.chequer.config;
 
 import com.chequer.domain.auth.CustomUserDetail;
-import com.chequer.domain.auth.Role;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
 
-import java.util.Collection;
 import java.util.Optional;
 
 @EnableJpaAuditing
@@ -21,9 +17,9 @@ public class AuditorAwareConfig implements AuditorAware<Long> {
         return Optional.ofNullable(SecurityContextHolder.getContext())
                 .map(SecurityContext::getAuthentication)
                 .map(authentication -> {
-                    Collection<? extends GrantedAuthority> auth = authentication.getAuthorities();
-                    boolean isUser = auth.contains(new SimpleGrantedAuthority(Role.USER.getKey()));
-                    if (isUser) return ((CustomUserDetail) authentication.getPrincipal()).getUserId();
+                    if (authentication.getPrincipal() instanceof CustomUserDetail) {
+                        return ((CustomUserDetail) authentication.getPrincipal()).getUserId();
+                    }
                     return null;
                 });
     }
